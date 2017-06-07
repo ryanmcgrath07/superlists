62d5504d265d29f07b04fe5089dda3a59d35d412
Ryan <ryanmcgrath07@gmail.com>
2017-06-06 21:15:43 -0400

    Use Bootstrap to improve layout

--- lists/urls.py
+++ lists/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from lists import views
+from lists import views 
 
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
